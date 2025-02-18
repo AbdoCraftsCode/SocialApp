c04c877f9fa4710b265be773a16926eb026a5360
abdo <wevaacademy@gmail.com>
2025-02-17 19:31:29 -0800

    sucsees

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -5,7 +5,7 @@ import { error, log } from "node:console";
 export const connectDB = async() => {
     return await mongoose.connect(process.env.DB_URL).then(res => {
         
-        console.log("DB connected sucssesfly mr abdo");
+        console.log("DB connected sucssesfly mr abdo tmm");
         
     }).catch(error => {
         
